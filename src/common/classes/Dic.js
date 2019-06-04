f581a65562b7fd7ab22673df3f5c1df53fee6a85
ledzefflin@gmail.com <ledzefflin@gmail.com>
2019-06-04 22:53:22 +0900

    [add] Dic

--- src/common/classes/Dic.js
+++ src/common/classes/Dic.js
@@ -2,20 +2,60 @@ export const Dic = class {
   constructor (obj) {
     _.forIn(obj, (v, k) => (this[k] = v));
   }
+
   key (v) {
     return _.invert(this)[v] || '';
   }
+
   set (k, v) {
     this[k] = v;
   }
+
   keys () {
     return _.keys(this);
   }
+
   values () {
     return _.values(this);
   }
+
   size () {
     return _.size(this);
+  }
+
+  map (fn) {
+    const mapper = _.flow([
+      f =>
+        _.reduce(
+          this,
+          (acc, v, k) => {
+            acc[k] = f(v);
+            return acc;
+          },
+          {}
+        ),
+      o => new Dic(o)
+    ]);
+    const newDic = mapper(fn);
+    return newDic;
+  }
+
+  filter (fn) {
+    const filter = _.flow([
+      f =>
+        _.reduce(
+          this,
+          (acc, v, k) => {
+            const r = f(v);
+            _.isBoolean(r) && _.isEqual(r, true) && (acc[k] = v);
+            return acc;
+          },
+          {}
+        ),
+      o => new Dic(o)
+    ]);
+    const newDic = filter(fn);
+    return newDic;
   }
 };
 
